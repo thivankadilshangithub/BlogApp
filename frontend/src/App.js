48659417ad3f85843416686979826015e8bd0779
thivankadilshangithub <thivankadilshan726@gmail.com>
2022-05-07 00:14:27 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Header from './components/Header';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Auth from './components/Auth';
+import Blogs from './components/Blogs';
+import UserBlog from './components/UserBlog';
+import BlogDetail from './components/BlogDetail';
+import AddBlog from './components/AddBlog';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { authActions } from './store';
+
+function App() {
+
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state=> state.isLoggedIn);
+  console.log(isLoggedIn);
+
+  useEffect(() => {
+    if(localStorage.getItem("userId"))
+    {
+        dispatch(authActions.login());
+    }
+  }, [dispatch]);
+
+  return (
+    <React.Fragment>
+      <header>
+      <Header />
+      </header>
+   
+    <main>
+    <Routes>
+     {  !isLoggedIn ?  <Route path='/auth' element={<Auth />} /> :
+     <>
+      <Route path='/blogs' element={<Blogs />} />
+      <Route path='/blogs/add' element={<AddBlog />} />
+      <Route path='/myBlogs' element={<UserBlog />} />
+      <Route path='/myBlogs/:id' element={<BlogDetail />} /></>
+     }
+     </Routes>
+    </main>
+    </React.Fragment>
+  )
+}
+
+export default App;
